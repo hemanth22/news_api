aee41bd38c7f4d8fdb490a1b52094f6365d41ac9
hemanth22 <hemanth22hemu@gmail.com>
2025-09-21 13:40:54 +0000

    feat: newsapi script

--- newsAPI_everything_basic_v2.py
+++ newsAPI_everything_basic_v2.py
@@ -2,7 +2,7 @@ import requests
 import os
 
 # Replace 'YOUR_API_KEY' with your actual News API key
-api_key =  os.environ.get('NEWS_API')
+api_key =  os.environ.get('NEWSAPI_ORG_KEY')
 
 # Specify the base URL for the News API 
 base_url = 'https://newsapi.org/v2/'
@@ -12,6 +12,9 @@ endpoint = 'everything'
 
 # Define list of countries
 queries = ['USD', 'SGD', 'INR']
+
+# Define webhook url
+webhook_url = "http://localhost:8000/newsapiwebhook"
 
 for query in queries:
     print("News Country: ",f"{query}","\n")
@@ -33,22 +36,26 @@ for query in queries:
     if response.status_code == 200:
     # Parse the JSON response
         data = response.json()
+        for article in data.get('articles', []):
+            payload = {
+                "source": "NewsAPI.org",
+                "title": article.get('title', 'N/A'),
+                "description": article.get('description', 'N/A'),
+                "url": article.get('url', 'N/A'),
+                "publishedTime": article.get('publishedAt', 'N/A'),
+                "sourcename": article.get("source", {}).get("name", "N/A"),
+                "author": article.get('author', 'N/A')
+            }
+#            print("JSON News:",payload)
+            try:
+               #print(payload)
+               response = requests.post(webhook_url, json=payload)
+               if response.status_code == 200:
+                   print(f"✅ Sent data for {payload['source']}")
+               else:
+                  print(f"⚠️ Failed to send {payload['symbol']}: {response.status_code} - {response.text}")
+            except requests.RequestException as e:
+                print(f"❌ Error sending data for {payload['symbol']}: {e}")
 
-    # Check if there are articles in the response
-        if 'articles' in data:
-            articles = data['articles']
-
-        # Print the headlines and descriptions of the articles
-            for article in articles:
-                html_format = '<a href="{}" target="_blank">{}</a>'.format(article["url"], article["title"])
-                print(article['description'],'|',article['source']['name'],'|' , html_format)
-#        for article in articles:
-#            print("Title:", article['title'])
-#            print("Description:", article['description'])
-#            print("Source:", article['source']['name'])
-#            print("URL:", article['url'])
-#            print("\n")
-        else:
-            print("No articles found in the response.")
-    else:
-        print("Error:", response.status_code)
+    if response.status_code != 200:
+        print("Failed to establish connection with NewsAPI.org")
