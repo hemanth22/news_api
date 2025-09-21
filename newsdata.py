0093ffb1099227b551607813a3dd0c358fc50324
hemanth22 <hemanth22hemu@gmail.com>
2025-09-21 15:15:26 +0000

    feat: newsdata.io

---
+++ newsdata.py
@@ -0,0 +1,35 @@
+import requests
+import os
+
+# Replace with your actual API key from NewsData.io
+
+API_KEY =  os.environ.get('NEWSDATA_IO_KEY')
+
+# Base URL
+BASE_URL = 'https://newsdata.io/api/1/news'
+
+# Keywords to search
+keywords = ['SocGen', 'DBS', 'StanChart']
+
+# Loop through keywords and query the API
+for keyword in keywords:
+    params = {
+        'apikey': API_KEY,
+        'q': keyword,
+        'language': 'en'
+    }
+
+    response = requests.get(BASE_URL, params=params)
+    data = response.json()
+
+    print(f"\n🔍 Results for: {keyword}")
+    print("Printing Data", data)
+    
+
+#    for article in data.get('results', []):
+#        print("Title:", article.get('title', 'N/A'))
+#        print("Description:", article.get('description', 'N/A'))
+#        print("URL:", article.get('link', 'N/A'))
+#        print("Published At:", article.get('pubDate', 'N/A'))
+#        print("Source Name:", article.get('source_id', 'N/A'))
+#        print("-" * 80)
